6b3005e7d870353deba6560b0ffc16e5f80dc5d2
Anna Manilaci <152987755+amanilac@users.noreply.github.com>
2024-07-23 11:35:46 +0300

    vog retry fixed norme

--- deps/libft/src/ft_strcmp.c
+++ deps/libft/src/ft_strcmp.c
@@ -6,15 +6,15 @@
 /*   By: amanilac <amanilac@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/25 19:19:33 by amanilac          #+#    #+#             */
-/*   Updated: 2024/06/25 19:40:12 by amanilac         ###   ########.fr       */
+/*   Updated: 2024/07/22 12:39:39 by amanilac         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../libft.h"
 
-int ft_strcmp(const char *str1, const char *str2)
+int	ft_strcmp(const char *str1, const char *str2)
 {
-	size_t i;
+	size_t	i;
 
 	i = 0;
 	while ((str1[i] || str2[i]))
